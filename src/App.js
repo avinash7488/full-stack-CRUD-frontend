6ef4004cbab6b9f6ce84cee600b2140d0f4db672
avinash kumar <avi064448@gmail.com>
2023-02-19 22:54:01 +0530

    correction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import AllRoutes from './components/AllRoutes';
-import Home from './components/Home';
+
 
 function App() {
   return (
